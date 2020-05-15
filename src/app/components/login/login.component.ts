27ac875847beff14598765bd2902e358f0244390
schaud <schuad24@gmail.com>
2020-05-15 17:51:04 -0400

    changed input for password (text -> password)

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,33 @@
+import {Component, OnInit, ElementRef, ViewChild} from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  @ViewChild('login') x: ElementRef;
+  @ViewChild('register') y: ElementRef;
+  @ViewChild('btn') z: ElementRef;
+
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+   switch1(){
+    this.x.nativeElement.style.left = '-400px';
+    this.y.nativeElement.style.left = '50px';
+    this.z.nativeElement.style.left = '110px';
+  }
+
+   switch2(){
+    this.x.nativeElement.style.left = '50px';
+    this.y.nativeElement.style.left = '450px';
+    this.z.nativeElement.style.left = '0';
+  }
+
+
+}
