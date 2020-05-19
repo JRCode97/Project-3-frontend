ba88387cf0adaa721f30c6c2004b086157b2be59
schaud <schuad24@gmail.com>
2020-05-19 10:57:56 -0400

    fixed errors

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {NewBugReportComponent} from './components/new-bug-report/new-bug-report.component'
-import {TestComponent} from './components/test/test.component'
+import {NewBugReportComponent} from './components/new-bug-report/new-bug-report.component';
+import {TestComponent} from './components/test/test.component';
+import {LoginComponent} from './components/login/login.component';
+
 
 const routes: Routes = [
-    {path:'newbugreport', component:NewBugReportComponent},
-    {path:'test', component:TestComponent}
+    {path: 'newbugreport', component: NewBugReportComponent},
+    {path: 'test', component: TestComponent},
+    {path: '', component: LoginComponent}
 ];
 
 @NgModule({
@@ -13,13 +16,7 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 
-export class AppRoutingModule { }
 
-import { LoginComponent} from './components/login/login.component';
-
-const routes: Routes = [
-  {path: 'login', component: LoginComponent}
-];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes), RouterModule],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import {MatInputModule} from '@angular/material/input';
 import {MatSelectModule} from '@angular/material/select';
 import { EditorModule} from '@tinymce/tinymce-angular';
-
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NewBugReportComponent } from './components/new-bug-report/new-bug-report.component';
@@ -13,11 +12,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatSliderModule} from '@angular/material/slider';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { TestComponent } from './components/test/test.component';
-import { MatSliderModule} from '@angular/material/slider';
-import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
-import { AppRoutingModule } from './app-routing.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MainNavComponent } from './components/main-nav/main-nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -30,7 +25,7 @@ import { MatListModule } from '@angular/material/list';
   declarations: [
     AppComponent,
     NewBugReportComponent,
-    TestComponent
+    TestComponent,
     LoginComponent,
     MainNavComponent,
   ],
@@ -45,7 +40,7 @@ import { MatListModule } from '@angular/material/list';
     MatInputModule,
     MatSelectModule,
     FormsModule,
-    EditorModule
+    EditorModule,
     BrowserAnimationsModule,
     MatSliderModule,
     LayoutModule,
