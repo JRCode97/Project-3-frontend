162983d0884911f3bae612fc4929641d683fe43f
Will Newell <newellwm@gmail.com>
2020-05-19 20:51:26 -0400

    organize app module imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,82 +1,82 @@
 import '@angular/compiler';
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { AppRoutingModule } from './app-routing.module';
-import {MatInputModule} from '@angular/material/input';
-import {MatSelectModule} from '@angular/material/select';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatButtonModule } from '@angular/material/button';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 import { EditorModule, TINYMCE_SCRIPT_SRC} from '@tinymce/tinymce-angular';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { MatFormFieldModule } from '@angular/material/form-field';
+import { AppRoutingModule } from './app-routing.module';
 import { MatSliderModule} from '@angular/material/slider';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MainNavComponent } from './components/main-nav/main-nav.component';
-import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
-import { MatTableModule } from '@angular/material/table';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { HttpClientModule } from '@angular/common/http';
-import { MatSortModule } from '@angular/material/sort';
 
-import { NewBugReportComponent } from './components/new-bug-report/new-bug-report.component';
-import { BugReportViewComponent } from './components/bug-report-view/bug-report-view.component';
-import { BugReportDetailsComponent } from './components/bug-report-details/bug-report-details.component';
-import { PostedSolutionsTableComponent } from './components/posted-solutions-table/posted-solutions-table.component';
 import { ApplicationComponent } from './components/application/application.component';
 import { ApplicationsTableComponent } from './components/application/applications-table/applications-table.component';
-import { MainPageBugComponent } from './components/main-page-bug/main-page-bug.component';
+import { BugReportViewComponent } from './components/bug-report-view/bug-report-view.component';
+import { BugReportDetailsComponent } from './components/bug-report-details/bug-report-details.component';
 import { MainPageApplicationComponent } from './components/main-page-application/main-page-application.component';
-import { MainPageLeaderboardComponent } from './components/main-page-leaderboard/main-page-leaderboard.component';
+import { MainPageBugComponent } from './components/main-page-bug/main-page-bug.component';
 import { MainPageComponent } from './components/main-page/main-page.component';
-import { MatExpansionModule } from '@angular/material/expansion';
+import { MainPageLeaderboardComponent } from './components/main-page-leaderboard/main-page-leaderboard.component';
+import { NewBugReportComponent } from './components/new-bug-report/new-bug-report.component';
+import { PostedSolutionsTableComponent } from './components/posted-solutions-table/posted-solutions-table.component';
+
 @NgModule({
   declarations: [
     AppComponent,
-    NewBugReportComponent,
-    LoginComponent,
-    MainNavComponent,
-    BugReportViewComponent,
-    BugReportDetailsComponent,
-    PostedSolutionsTableComponent,
     ApplicationComponent,
     ApplicationsTableComponent,
-    MainPageBugComponent,
+    BugReportViewComponent,
+    BugReportDetailsComponent,
+    LoginComponent,
+    MainNavComponent,
     MainPageApplicationComponent,
+    MainPageComponent,
+    MainPageBugComponent,
     MainPageLeaderboardComponent,
-    MainPageComponent
+    NewBugReportComponent,
+    PostedSolutionsTableComponent
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
+    BrowserModule,
+    EditorModule,
+    FormsModule,
+    HttpClientModule,
+    LayoutModule,
+    MatButtonModule,
+    MatExpansionModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatPaginatorModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSliderModule,
+    MatSortModule,
+    MatTableModule,
+    MatToolbarModule,
     NgbModule,
     ReactiveFormsModule,
-    BrowserAnimationsModule,
-    MatSliderModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatSelectModule,
-    FormsModule,
-    EditorModule,
-    LayoutModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule,
-    MatTableModule,
-    MatPaginatorModule,
-    MatSortModule,
-    HttpClientModule,
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatExpansionModule
   ],
   providers: [{ provide: TINYMCE_SCRIPT_SRC, useValue: 'tinymce/tinymce.min.js' }],
   bootstrap: [AppComponent]
