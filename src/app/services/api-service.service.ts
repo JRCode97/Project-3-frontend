f3dddbcbd41650e081b5de72e7c1a7f94ae2d1bf
unknown <wsd81_2@hotmail.com>
2020-05-21 18:41:23 -0400

    fix the create new solution submit error

--- src/app/services/api-service.service.ts
+++ src/app/services/api-service.service.ts
@@ -13,8 +13,8 @@ export class ApiServiceService {
 
   constructor(private http: HttpClient) { }
 
-  //path: string = 'http://ec2-52-14-153-164.us-east-2.compute.amazonaws.com:9000'
-   path: string = 'http://localhost:9000'
+   path: string = 'http://ec2-52-14-153-164.us-east-2.compute.amazonaws.com:9000'
+  // path: string = 'http://localhost:9000'
 
 
 
