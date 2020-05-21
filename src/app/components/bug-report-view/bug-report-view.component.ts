2ecd3769727debdf7a38aac1877b2df5774c6579
unknown <wsd81_2@hotmail.com>
2020-05-21 18:48:27 -0400

    fix the create new solution submit error

--- src/app/components/bug-report-view/bug-report-view.component.ts
+++ src/app/components/bug-report-view/bug-report-view.component.ts
@@ -4,7 +4,7 @@ import Solution from 'src/app/models/Solution';
 import BugReport from 'src/app/models/BugReport';
 import Client from 'src/app/models/Client';
 import { ActivatedRoute, Router } from '@angular/router';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 @Component({
     selector: 'app-bug-report-view',
     templateUrl: './bug-report-view.component.html',
@@ -18,13 +18,12 @@ export class BugReportViewComponent implements OnInit {
     public client: Client;
     public SolDescription: string = '';
     public SolTitle: string = '';
-    registerForm: FormGroup;
-    submitted = false;
 
 
 
 
-    constructor(private apiserv: ApiServiceService, private route: ActivatedRoute, private router: Router, private formBuilder: FormBuilder) {
+
+    constructor(private apiserv: ApiServiceService, private route: ActivatedRoute, private router: Router) {
         // const queryString = window.location.search;
         // const urlParams = new URLSearchParams(queryString);
         // this.brId = urlParams.get("brid");
@@ -42,36 +41,6 @@ export class BugReportViewComponent implements OnInit {
     }
 
     ngOnInit(): void {
-        this.registerForm = this.formBuilder.group({
-            SolDescription: ['', [Validators.required, Validators.minLength(100)]],
-            txtSolTitle: ['', [Validators.required, Validators.minLength(6)]]
-        });
-    }
-    // convenience getter for easy access to form fields
-    get f() { return this.registerForm.controls; }
-
-    onSubmit() {
-        this.submitted = true;
-        console.log(this.registerForm.invalid);
-        // stop here if form is invalid
-        if (this.registerForm.invalid) {
-
-            return;
-        }
-
-       else if (!this.registerForm.invalid) {
-            this.postSolution();
-        }
-    
-
-        // display form values on success
-        alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.registerForm.value, null, 4));
-    }
-
-
-    onReset() {
-        this.submitted = false;
-        this.registerForm.reset();
     }
     //0. Get Client By ID 
     getClient(): Client {
@@ -96,6 +65,7 @@ export class BugReportViewComponent implements OnInit {
     }
     //3. Add new  Solution 
     async postSolution(): Promise<any> {
+        
         let sol = new Solution();
         console.log(this.br);
         sol.br = this.br;
