9858f218024067ec4b4fc2c883489a932f00550a
Kurt Dawiec <kurtmdawiec@gmail.com>
2020-09-01 15:16:21 -0400

    changed stuff

--- src/app/components/main-page/main-page.component.ts
+++ src/app/components/main-page/main-page.component.ts
@@ -7,7 +7,7 @@ import { OverlayContainer} from '@angular/cdk/overlay';
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.css']
+  styleUrls: ['./main-page.component.scss']
 })
 export class MainPageComponent implements OnInit {
 
