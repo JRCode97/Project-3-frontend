f821bf0c2c13b3749e7014e107799ca6efa70260
mnauman319 <mnauman319@gmail.com>
2020-09-02 16:02:37 -0500

    Allowed unregistered users to see resolved bugs on view bugs page.

--- src/app/components/view-bugs-page/view-bugs-page.component.ts
+++ src/app/components/view-bugs-page/view-bugs-page.component.ts
@@ -10,11 +10,13 @@ import {Client} from 'src/app/models/Client';
 export class ViewBugsPageComponent implements OnInit {
   isAdmin:boolean;
   client:Client;
+  isDeveloper:boolean;
   constructor(private serv: ApiServiceService) { }
 
   ngOnInit(): void {
     this.client = this.serv.getLoggedClient();
     this.isAdmin = this.client.role ? true : false;
+    this.isDeveloper = this.client.cId ? true : false;
   }
 
 }
